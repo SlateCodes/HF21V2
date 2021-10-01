5a58768280585483a62aaa657968e44cce79a13a
Brad <44570552+Iskawo@users.noreply.github.com>
2021-10-01 02:29:50 +0100

    error logging, fix async

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-function main(args) {
+async function main(args) {
     const { execSync } = require("child_process");
-    const result = await execSync(args).toString();
-    return result;
+    let result;
+    try {
+        result = await execSync(args).toString();
+    } catch (e) {
+        result = e;
+    }
+    return result.message ? result.message : result
 }
-main("ls");
+await main("ls");
